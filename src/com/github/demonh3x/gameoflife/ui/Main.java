41f889b600c582f60916009237a56aca124c4dd3
mateu <mateu@touchiteasy.com>
2014-05-18 15:18:48 +0200

    Construct the view window locations only once

--- src/com/github/demonh3x/gameoflife/ui/Main.java
+++ src/com/github/demonh3x/gameoflife/ui/Main.java
@@ -5,24 +5,35 @@ import com.github.demonh3x.gameoflife.*;
 public class Main {
     public static void main (String ... args) throws InterruptedException {
         final GameOfLife gameOfLife = new GameOfLife(new InitialGeneration(new RandomCellFactory()));
+        BidimensionalLocation[][] locationsWindow = constructWorldWindow(20, 77);
 
         for (int i = 0; i < 100; i++){
-            final String screen = toString(gameOfLife.tick());
+            final String screen = toString(gameOfLife.tick(), locationsWindow);
             System.out.println(screen);
             Thread.sleep(10);
         }
     }
 
-    private static String toString(Generation gen) {
-        String s = "\n\n\n\n";
-        int rows = 20;
-        int cols = 77;
+    private static BidimensionalLocation[][] constructWorldWindow(int rows, int cols){
+        BidimensionalLocation[][] locations = new BidimensionalLocation[rows][cols];
 
         for (int x = 0; x < rows; x++){
-            s += " ";
             for (int y = 0; y < cols; y++){
-                final Cell cell = gen.lookAt(new BidimensionalLocation(x, y));
-                s += cell instanceof AliveCell? '#': ' ';
+                locations[x][y] = new BidimensionalLocation(x, y);
+            }
+        }
+
+        return locations;
+    }
+
+    private static String toString(Generation gen, BidimensionalLocation[][] locations) {
+        String s = "\n\n\n\n";
+
+        for (BidimensionalLocation[] rows : locations) {
+            s += " ";
+            for (BidimensionalLocation location : rows) {
+                final Cell cell = gen.lookAt(location);
+                s += cell instanceof AliveCell ? '#' : ' ';
             }
             s += "\n";
         }
