d71d62feff7165dc3ebc92f809676231906d9b7a
mateu <mateu@touchiteasy.com>
2014-05-20 10:07:07 +0200

    Modified the main to be more performant with the new SnapshotGeneration and show performance information.

--- src/com/github/demonh3x/gameoflife/ui/Main.java
+++ src/com/github/demonh3x/gameoflife/ui/Main.java
@@ -2,15 +2,69 @@ package com.github.demonh3x.gameoflife.ui;
 
 import com.github.demonh3x.gameoflife.*;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main (String ... args) throws InterruptedException {
-        final GameOfLife gameOfLife = new GameOfLife(new InitialGeneration(new RandomCellFactory()));
-        BidimensionalLocation[][] locationsWindow = constructWorldWindow(20, 77);
+        final int generationsToRender = args.length >= 1? Integer.parseInt(args[0]) : 200;
+        final int msPerFrame = args.length >= 2? Integer.parseInt(args[1]): 200;
+        final int framesPerClean = args.length >= 3? Integer.parseInt(args[2]): 10;
 
-        for (int i = 0; i < 100; i++){
-            final String screen = toString(gameOfLife.tick(), locationsWindow);
-            System.out.println(screen);
-            Thread.sleep(10);
+        Generation gen = new InitialGeneration(new RandomCellFactory());
+        BidimensionalLocation[][] locationsWindow = constructWorldWindow(20, 77);
+        Location[] locations = flatten(locationsWindow);
+
+        ArrayList<Integer> renderTimes = new ArrayList<Integer>();
+        for (int i = 0; i <= generationsToRender; i++){
+            long start, end;
+            start = System.currentTimeMillis();
+
+            final String screen = toString(gen, locationsWindow);
+
+            if (i % framesPerClean == 0){
+                gen = new SnapshotGeneration(gen, locations, new DeadCellFactory());
+            }
+            gen = new NextGeneration(gen);
+
+            end = System.currentTimeMillis();
+            long renderTime = end - start;
+            renderTimes.add((int) renderTime);
+
+            long maxFps = 1000 / renderTime;
+            long actualFps = 1000 / (renderTime > msPerFrame? renderTime : msPerFrame);
+
+            System.out.println(screen + String.format(
+                    "Frame: %s/%s, RenderTime: %sms, FrameTime: %sms FPS: %s(actual)/%s(potential)",
+                    i, generationsToRender, renderTime, msPerFrame, actualFps, maxFps)
+            );
+
+            long msToWait = msPerFrame - renderTime;
+            if (msToWait > 0)
+                Thread.sleep(msToWait);
+        }
+
+        Statistics s = new Statistics(renderTimes);
+
+        System.out.println(String.format(
+                "Average render time: %sms, Min: %sms, Max: %sms",
+                s.average, s.min, s.max
+        ));
+    }
+
+    private static class Statistics {
+        public final long average, sum, min, max;
+
+        private Statistics(ArrayList<Integer> renderTimes) {
+            long sum = 0, min = Long.MAX_VALUE, max = Long.MIN_VALUE;
+            for (int i : renderTimes){
+                sum += i;
+                if (i < min) min = i;
+                if (i > max) max = i;
+            }
+            this.sum = sum;
+            this.average = (sum / renderTimes.size());
+            this.min = min;
+            this.max = max;
         }
     }
 
@@ -26,6 +80,17 @@ public class Main {
         return locations;
     }
 
+    private static Location[] flatten(BidimensionalLocation[][] locations){
+        Location[] ret = new Location[locations.length * locations[0].length];
+
+        int i = 0;
+        for (Location[] row : locations)
+            for (Location l : row)
+                ret[i++] = l;
+
+        return ret;
+    }
+
     private static String toString(Generation gen, BidimensionalLocation[][] locations) {
         String s = "\n\n\n\n";
 
@@ -33,7 +98,7 @@ public class Main {
             s += " ";
             for (BidimensionalLocation location : rows) {
                 final Cell cell = gen.lookAt(location);
-                s += cell instanceof AliveCell ? '#' : ' ';
+                s += cell instanceof AliveCell ? 'O' : ' ';
             }
             s += "\n";
         }
